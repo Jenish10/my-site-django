9060ec2a8b7e5b0e5de99b4b68fc7097c8eaa3eb
JenishAcharya <jenish.acharya.c5@gmail.com>
2019-04-22 17:03:06 +0545

    result page added. validations added while voting.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404 ,render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
+from django.urls import reverse
 
 from .models import Question
 
@@ -23,7 +24,7 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise Http404("Question does not exist.")        
     print (request)
-    return HttpResponse("You're looking at question %s. Nice" % question_id)
+    # return HttpResponse("You're looking at question %s. Nice" % question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
@@ -31,6 +32,24 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing with POST data.
+        # This prevents data from being posted twice if a user hits the back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
+    # return HttpResponse("You're voting on question %s." % question_id)
+    
+    
 
